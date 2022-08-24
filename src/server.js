e0fb21845968bea420926aa51ad20ddd4a58cabe
josebernal12 <106898047+josebernal12@users.noreply.github.com>
2022-08-24 17:07:53 -0600

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -1,140 +1,61 @@
-const express = require("express");
-const bcrypt = require("bcrypt");
-const session = require("express-session");
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const routes = require("./routes/rutas.js");
-const config = require("./config/config.js");
-const controllersdb = require("./controllers/controller.js");
-const User = require("./model/model.js");
+require('dotenv').config()
+const express = require('express');
+const path = require('path');
+const engine = require('ejs-mate');
+const session = require('express-session');
+const passport = require('passport');
+const controllersdb = require('./database/database')
+const port = process.env.PORT || 8080;
+
+
 const app = express();
-const path = require('path')
-
-const port = 3000;
+require('./passport/passport');
 
 
 
-app.use(express.static(path.join(__dirname, "./views")))
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, './views'))
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname,'./views')))
 
-app.use(
-  session({
-    secret: "coderhouse",
-    cookie: {
-      httpOnly: false,
-      secure: false,
-      maxAge: config.TIEMPO_EXPIRACION,
-    },
-    rolling: true,
-    resave: false,
-    saveUninitialized: false,
-  })
-);
+
+
+app.use(express.urlencoded({extended: false}));
+app.use(session({
+  secret: 'SecretJose',
+  resave: false,
+  saveUninitialized: false,
+  rolling:true
+  
+
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-function hashPassword(password) {
-  return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-}
-
-function isValidPassword(reqPassword, hashedPassword) {
-  return bcrypt.compareSync(reqPassword, hashedPassword);
-}
-
-const signupStrategy = new LocalStrategy(
-  { passReqToCallback: true },
-  async (req, username, password, done) => {
-    try {
-      const existingUser = await User.findOne({ username });
-
-      if (existingUser) {
-        return done("User already exists", false);
-      }
-
-      const newUser = {
-        username: username,
-        password: hashPassword(password),
-
-      }
-
-      const createdUser = await User.create(newUser);
-
-      return done(null, createdUser);
-    } catch (err) {
-      console.log(err);
-      done(err);
-    }
-  }
-);
-
-const loginStrategy = new LocalStrategy(async (username, password, done) => {
-  const user = await User.findOne({ username });
-
-  if (!user || !isValidPassword(password, user.password)) {
-    return done("Invalid credentials", null);
-  }
-
-  return done(null, user);
+app.use((req, res, next) => {
+  app.locals.user = req.user;
+    next();
 });
 
-passport.use("register", signupStrategy);
-passport.use("login", loginStrategy);
 
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
-
-passport.deserializeUser((id, done) => {
-  User.findById(id, done);
-});
-
-function checkauth(req, res, next) {
-  if (req.isAuthenticated()) {
-    next()
-  }else{
-    res.redirect('/login')
-  }
-}
+app.use('/', require('./routes/index'));
+app.use((req,res) =>{
+ res.render('failpage')
+ })
 
 
-app.get("/register", routes.getSignup);
-
-app.post(
-  "/register",
-  passport.authenticate("register", { failureRedirect: "/failsignup" }),
-  routes.postSignup
-);
-
-app.get("/failsignup", routes.getFailsignup);
-
-app.get("/login", routes.getLogin);
-app.post(
-  "/login",
-  passport.authenticate("login", { failureRedirect: "/faillogin" }),
-  routes.postLogin
-)
-app.get("/faillogin", routes.getFaillogin);
-
-app.get('/logout', routes.getlogout);
-
-app.get('/nombre' , (req,res)=>{
-  const mensaje = User.username 
-  res.json(mensaje)
-})
-
-
-
-
-
-controllersdb.conectarDB("mongodb://localhost:27017/passport", (err) => {
+controllersdb.conectarDB(process.env.URL, (err) => {
   if (err) return console.log("error en conexión de base de datos", err);
   console.log("BASE DE DATOS CONECTADA");
-
-  app.listen(port, (err) => {
-    if (err) return console.log("error en listen server", err);
-    console.log(`Server running on port ${port}`);
-  });
-
 })
+
+app.listen(port, (error) => {
+  if (error) {
+      console.log(error)
+  } else {
+      console.log(`Servidor escuchando puerto: ${port}`)
+  }
+
+});
+
